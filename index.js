3875add1d02fd0e1d35828fdd97ca887cb77f19a
Lee <lee.o.noble@gmail.com>
2018-07-05 09:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -133,6 +133,7 @@ function moveDodgerLeft() {
   var left = parseInt(leftNumbers, 10)
   if (left > 0) {
     DODGER.style.left = `${left - 4}px`
+    window.requestAnimationFrame(moveDodgerLeft);
   }
 }
 
@@ -142,6 +143,7 @@ function moveDodgerRight() {
    var left = parseInt(leftNumbers, 10)
    if (left < 360) {
      DODGER.style.left = `${left + 4}px`
+     window.requestAnimationFrame(moveDodgerRight);
    }
  }
 
