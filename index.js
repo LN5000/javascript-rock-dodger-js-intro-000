249740475a6937041ebed3d15b256760689c9334
Lee <lee.o.noble@gmail.com>
2018-07-05 09:46:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-/**
- * Don't change these constants!
- */
-const DODGER = document.getElementById('dodger')
-const GAME = document.getElementById('game')
-const GAME_HEIGHT = 400
-const GAME_WIDTH = 400
-const LEFT_ARROW = 37 // use e.which!
-const RIGHT_ARROW = 39 // use e.which!
-const ROCKS = []
-const START = document.getElementById('start')
 
-var gameInterval = null
-/**
- * Don't change these constants!
- */
 const DODGER = document.getElementById('dodger')
 const GAME = document.getElementById('game')
 const GAME_HEIGHT = 400
